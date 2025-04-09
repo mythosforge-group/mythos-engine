4667f51ea71cf1e2f1d28cd0499c2dd57f517b4d
Ianco <ianco.soares.labcomu@imd.ufrn.br>
2025-04-09 12:46:21 -0300

    :tada: first commit

---
+++ src/main/java/mythosforge/fable_minds/FableMindsApplication.java
@@ -0,0 +1,13 @@
+package mythosforge.fable_minds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FableMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FableMindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mythosforge/fable_minds/FableMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package mythosforge.fable_minds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FableMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
