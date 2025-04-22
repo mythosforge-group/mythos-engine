d8909bb550a5cd51688ef0fbd0eaee1e58af1e9b
ianco <ianco.soares.labcomu@imd.ufrn.br>
2025-04-21 23:07:35 -0300

    :ok_hand: style: mudando models para padrão lambok

--- src/main/java/mythosforge/fable_minds/models/ErrorResponse.java
+++ src/main/java/mythosforge/fable_minds/models/ErrorResponse.java
@@ -1,29 +1,7 @@
 package mythosforge.fable_minds.models;
-
+import lombok.Data;
+@Data
 public class ErrorResponse {
-    private String message;
-    private String details;
-
-    // Construtor público
-    public ErrorResponse(String message, String details) {
-        this.message = message;
-        this.details = details;
-    }
-
-    // Getters e setters
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
+    private final String message;
+    private final String details;
 }
