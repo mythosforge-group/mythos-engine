ca758a6f6827ae0bf07e787564fa85112cdce0a2
ianco <ianco.soares.labcomu@imd.ufrn.br>
2025-07-02 10:48:51 -0300

    :bug: fix(build): corrigindo erro de build (dois métodos main)

--- src/main/java/mythosforge/fable_minds/FableMindsApplication.java
+++ src/main/java/mythosforge/fable_minds/FableMindsApplication.java
@@ -3,7 +3,11 @@ package mythosforge.fable_minds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "mythosforge.fable_minds",
+    "mythosengine.core"
+})
+
 public class FableMindsApplication {
 
 	public static void main(String[] args) {
